96ff777bc6e82036d67689f702e5402139c10829
Satan <lindorgiorvanny@gmail.com>
2022-06-14 22:53:00 -0400

    Initial Commit

---
+++ src/test/java/com/libby/xebru/XebruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libby.xebru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XebruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
